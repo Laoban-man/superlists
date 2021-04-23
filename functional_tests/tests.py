dac987dc0ff5db4be02f6fa6f944d58196d5c751
Laoban-man <nicolasmichaelroux@gmail.com>
2021-04-23 13:01:08 +0200

    hack ft runner to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-
+import os
 MAX_WAIT = 10
 
 
@@ -11,10 +11,16 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get("STAGING_SERVER")
+        if staging_server:
+            self.live_server_url="http://" + staging_server
 
     def tearDown(self):
         self.browser.quit()
 
+
+# Database
+# https://docs.djang
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
